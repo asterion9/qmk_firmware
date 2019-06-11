51bf374a4f6556e0fa314e6206157c201e2d1dd7
fesssstif <fesssstif@nowhere.com>
2019-06-11 23:38:52 +0200

    set settings for mouse on ergodone

--- keyboards/ergodone/keymaps/fesssstif/config.h
+++ keyboards/ergodone/keymaps/fesssstif/config.h
@@ -9,9 +9,10 @@
 #undef MOUSEKEY_MAX_SPEED
 #undef MOUSEKEY_WHEEL_MAX_SPEED
 
-#define MOUSEKEY_DELAY          50
+
 #define MOUSEKEY_INTERVAL       20
-#define MOUSEKEY_TIME_TO_MAX    30
-#define MOUSEKEY_MAX_SPEED      8
+#define MOUSEKEY_DELAY          20
+#define MOUSEKEY_TIME_TO_MAX    40
+#define MOUSEKEY_MAX_SPEED      9
 
 #define MOUSEKEY_WHEEL_MAX_SPEED 5
